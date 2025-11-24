9034b2725f260b98bdc9e1b6c9990683d682b13a
Kainan silva <ana.b.lima8@aluno.senai.br>
2025-11-24 15:43:16 -0300

    projeto inicio

---
+++ Atividades/Média escola/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a primeira nota");
+decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a segunda nota");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota");
+decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a quarta nota");
+decimal nota4 = Convert.ToDecimal(Console.ReadLine());
+
+
+decimal media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+Console.WriteLine($"A média é: {media} ");
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+
+
+
